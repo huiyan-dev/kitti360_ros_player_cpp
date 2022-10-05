2ceb64d1f433ec82b26c65ae94cba86ea90f4619
huiyan <lhy.fullstack@gmail.com>
2022-10-05 21:14:24 +0800

    chore : add default value for config

--- yan/include/common/config.h
+++ yan/include/common/config.h
@@ -18,25 +18,25 @@ struct Config {
 
   std::string home_dir{};
   std::string data_root_dir{"/Datasets/KITTI-360/"};
-  std::string data_imu_raw_dir{};
-  std::string data_2d_raw_dir{};
+  std::string data_imu_raw_dir{"data_poses_oxts_extract/"};
+  std::string data_2d_raw_dir{"data_2d_raw/"};
   std::string data_2d_raw_img_extension{".png"};
-  std::string data_3d_raw_dir{};
+  std::string data_3d_raw_dir{"data_3d_raw/"};
   std::string data_3d_raw_lidar_extension{".bin"};
-  std::string sequence_number{};
-  std::string sequence_sync{};
+  std::string sequence_number{"0003"};
+  std::string sequence_sync{""};
   std::string sequence_extract{};
 
-  std::string topic_name_left_perspective;
-  std::string topic_name_right_perspective;
-  std::string topic_name_lidar_velo;
+  std::string topic_name_left_perspective{"kitti360/data_2d_raw/left_perspective"};
+  std::string topic_name_right_perspective{"kitti360/data_2d_raw/right_perspective"};
+  std::string topic_name_lidar_velo{"kitti360/data_3d_raw/lidar_velo"};
 
   std::string frame_id_world{"world"};
   std::string frame_id_lidar{"lidar"};
   std::string frame_id_cam0{"cam0"};
   std::string frame_id_imu{"imu"};
 
-  std::string data_output_dir;
+  std::string data_output_dir{"output/"};
 
   Config() {
     home_dir = std::getenv("HOME");
